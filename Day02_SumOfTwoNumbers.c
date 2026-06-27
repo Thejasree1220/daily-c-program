5ba83ccd46f510bf168f60000cecc05f0dc3b036
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-06-27 08:26:17 +0530

    Day 2: Sum of Two Numbers in C

---
+++ Day02_SumOfTwoNumbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  int num1,num2,sum;
+printf("enter the first number and second number:");
+scanf("%d %d",&num1,&num2);
+sum=num1+num2;
+printf("sum=%d\n",sum);
+}
