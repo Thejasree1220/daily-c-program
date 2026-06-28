459da879d11036d7593848e0c68822fd34db32c2
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-06-28 08:14:41 +0530

    Day 3: Even or Odd Program in C

---
+++ Day03_EvenOdd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+printf("enter the num:");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("%d is even num\n",num);
+}
+else
+{
+printf("%d is odd num\n",num);
+}
+}
