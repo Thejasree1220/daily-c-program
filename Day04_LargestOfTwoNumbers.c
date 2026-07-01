056f162d9e7f56c3acd43084a026344df8d79a9c
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-01 06:53:50 +0530

    Day 4: Largest of Two Numbers in C

---
+++ Day04_LargestOfTwoNumbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int num1,num2;
+printf("enter the two numbers:");
+scanf("%d %d",&num1,&num2);
+if(num1>num2)
+{
+printf("%d is greter\n",num1);
+}
+else
+{
+printf("%d is greater\n",num2);
+}
+}
