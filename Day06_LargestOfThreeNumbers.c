06daf7818704e5ad7137b32a379afcad089a31ba
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-03 07:14:32 +0530

    Day 6: Largest of Three Numbers in C

---
+++ Day06_LargestOfThreeNumbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+printf("enter three numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("%d is largest number\n",a);
+}
+else if(b>a&&b>c)
+{
+printf("%d is largest number\n",b);
+}
+else
+{
+printf("%d is largest number\n",c);
+}
