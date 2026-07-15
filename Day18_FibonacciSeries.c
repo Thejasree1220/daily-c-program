be3f037f87eb8de6bf909f9a1e5aa1c9c60ab5ed
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-15 23:55:04 +0530

    Day 18: Fibonacci Series in C
    Implement Fibonacci series generator in C.

---
+++ Day18_FibonacciSeries.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int n, first = 0, second = 1, next, i;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series:\n");
+    for(i = 1; i <= n; i++)
+    {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    printf("\n");
+}
