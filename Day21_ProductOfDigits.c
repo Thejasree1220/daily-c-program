3f821ac779b29d6718bbce9afeb9d550000bc9c6
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-18 22:34:26 +0530

    Day 21: Product of Digits in C

---
+++ Day21_ProductOfDigits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int num, digit, product=1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(num;num;num=num/10)
+    {
+        digit = num % 10;
+        product *= digit;
+    }
+    printf("Product of digits = %lld\n", product);
+}
