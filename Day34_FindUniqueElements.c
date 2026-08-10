24e56ec741999c8b772430ae40bbfcb7912cc91f
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-08-10 23:21:35 +0530

    Day 34: Find Unique Elements in Array
    This program takes an array of integers as input and prints the unique elements that appear only once.

---
+++ Day34_FindUniqueElements.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+  void main()
+  {
+           int a[100], n, i, j, count;
+           printf("Enter number of elements: ");
+           scanf("%d", &n);
+           printf("Enter elements:\n");
+           for(i = 0; i < n; i++)
+                   scanf("%d", &a[i]);
+          printf("Unique elements: ");
+          for(i = 0; i < n; i++)
+          {
+                  count = 0;
+                  for(j = 0; j < n; j++)
+                  {
+                          if(a[i] == a[j])
+                                  count++;
+                  }
+  
+                  if(count == 1)
+                          printf("%d ", a[i]);
+          }
+          printf("\n");
+ }
